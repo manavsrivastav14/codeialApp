77c21aad9c90ecb5576935c9bce8a07cf5a76306
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-13 15:05:07 +0530

    Adding routing to project

--- codeial/src/components/App.js
+++ codeial/src/components/App.js
@@ -1,8 +1,21 @@
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { getPosts } from '../api';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 import { Loader, Navbar } from './';
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404</h1>;
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -29,7 +42,23 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home posts={posts} />
+      <Router>
+        <Route path="/">
+          <Home posts={posts} />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/user/asdasd">
+          <UserInfo />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Router>
+      <Route path="/">
+        <Page404 />
+      </Route>
     </div>
   );
 }
